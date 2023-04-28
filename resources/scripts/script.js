ad01217e2a8f354960c5260cdd7b61dd0cf307a1
habuham <130504891+habuham@users.noreply.github.com>
2023-04-28 08:54:26 -0500

    New version
    Most recent version containing functional JavaScript and color-coded victory or defeat messages.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -53,13 +53,36 @@ const playGame = (event) => {
   const userChoice = getUserChoice();
   const computerChoice = getComputerChoice();
 
+  // Labels the boxes with their respective choices.
   document.getElementById("playerBox").innerHTML = userChoice + '!';
   document.getElementById("computerBox").innerHTML = computerChoice + '!';
 
+  // Applies a class to each box for stylistic reasons.
   document.querySelector("#playerBox").classList.add("filled");
   document.querySelector("#computerBox").classList.add("filled");
 
-
-  return determineWinner(userChoice, computerChoice);
+  // Calls the function that determines the winner of a round.
+  let outcome = determineWinner(userChoice, computerChoice);
   
+  // Inserts the victory message in between the boxes.
+  document.getElementById("outcome").innerHTML = outcome;
+
+  // Appies a class to the outcome box for stylistic reasons.
+  switch (outcome) {
+    case 'You won!':
+      document.querySelector("#outcome").classList.add("win");
+      document.querySelector("#outcome").classList.remove("loss");
+      document.querySelector("#outcome").classList.remove("tie");
+      break;
+    case 'The computer won!':
+      document.querySelector("#outcome").classList.add("loss");
+      document.querySelector("#outcome").classList.remove("win");
+      document.querySelector("#outcome").classList.remove("tie");
+      break;
+    default:
+      document.querySelector("#outcome").classList.add("tie");
+      document.querySelector("#outcome").classList.remove("win");
+      document.querySelector("#outcome").classList.remove("loss");
+      break;
+  }
 };
